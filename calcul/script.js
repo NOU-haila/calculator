c78220adf45b8785177870f6ff1fc61ceed929f1
Nouhaila <117626009+NOU-haila@users.noreply.github.com>
2023-11-18 12:54:35 +0100

    Add files via upload

---
+++ calcul/script.js
@@ -0,0 +1,35 @@
+const touches = [...document.querySelectorAll('.bouton')];
+const listekeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('.ecran');
+
+document.addEventListener('keydown',(e) => {
+    const valeur = e.keyCode.toString();
+    calculet(valeur)
+
+})
+ document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
+
+ })
+
+ const calculer=(valeur) => {
+    if(listekeycode.includes(valeur)){
+        switch(valeur){
+            case'8':
+            ecran.textContent="";
+            break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent=calcul;
+                break;
+                default:
+                    const indexkeycode = listekeycode.indexOf(valeur);
+                   const touche = touches[indexkeycode];
+                   ecran.textContent += touche.innerHTML;
+        }
+    }
+ }
+ window.addEventListener('error',(e) => {
+    alert("Erreur dans votre calcul :"+e.message);
+ } )
